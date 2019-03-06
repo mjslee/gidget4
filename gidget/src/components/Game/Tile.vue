0efafd3cb438aaca86bb3119879c18d0a8cc0865
thetarkus <mike@mikemob.com>
2019-03-06 10:09:17 -0500

    Initial game world generator

---
+++ gidget/src/components/Game/Tile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :style="{ height: size + 'rem', width: size + 'rem' }"></div>
+</template>
+
+
+<style scoped>
+div {
+  border: 1px solid transparent;
+  display: inline-block;
+  background: green;
+}
+</style>
+
+
+<script>
+export default {
+  props: ['size']
+}
+</script>
