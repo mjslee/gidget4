a100f288948f4a4f552237904fc78fa1b5904087
thetarkus <mike@mikemob.com>
2019-03-06 10:09:17 -0500

    Initial game world generator

--- gidget/src/components/Game/World.vue
+++ gidget/src/components/Game/World.vue
@@ -1,19 +1,39 @@
 <template>
-  <main id="world">
-    
+  <main id="world" ref="world">
+    <div v-for="row in worldSize" :key="row"
+      :style="{ height: tileSize + 'rem' }" class="game-row">
+      <Tile v-for="col in worldSize" :key="col" :size="tileSize" />
+    </div>
   </main>
 </template>
 
-<style scoped>
-
-</style>
 
 <script>
-import GidgetEngine from '../../libraries/gidget-engine';
+import Engine from '../../libraries/gidget-engine'
+import Tile from './Tile.vue'
 
 
 export default {
+  components: {
+    Tile
+  },
 
+  created() {
+    // Create copy of engine
+    this.engine = Object.assign({}, Engine)
+    window.engine = this.engine  // Access in console for debugging purposes
+  },
+
+  methods: {
+
+  },
+
+  data() {
+    return {
+      worldSize: 4,
+      tileSize: 3
+    }
+  },
 }
 </script>
 
