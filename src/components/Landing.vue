f115b3a6e390a1f0ac23f7c68f1ebf63303f643b
RichardFebres <rf57@njit.edu>
2019-03-10 15:54:30 -0400

    Create basic route to view mapping for login functionality

---
+++ src/components/Landing.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container fluid>
+        <v-layout column>
+            <v-flex md12 class="text-md-center" mt-5>
+                <h1>Landing page</h1>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {}
+</script>
