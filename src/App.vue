50232f154c597f7d65e9f79799f348aaf377846d
RichardFebres <rf57@njit.edu>
2019-03-12 18:39:10 -0400

    Update App to reflect state addition and return App Title

--- src/App.vue
+++ src/App.vue
@@ -48,13 +48,17 @@
   export default {
     data () {
       return {
-        appTitle: 'Gidget',
         sidebar: false,
         menuItems: [
           { title: 'Home', path: '/home', icon: 'home' },
           { title: 'Sign Up', path:'/signup', icon: 'face' },
           { title: 'Sign In', path: '/signin', icon: 'lock_open' }
         ]
+      }
+    },
+    computed: {
+      appTitle () {
+        return this.$store.state.appTitle
       }
     }
   }
