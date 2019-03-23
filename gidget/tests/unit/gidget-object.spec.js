34a3eb0970848942732f9341765e7af01e15f6ec
thetarkus <mike@mikemob.com>
2019-03-23 12:41:29 -0400

    Add gidget object tests

--- gidget/tests/unit/gidget-object.spec.js
+++ gidget/tests/unit/gidget-object.spec.js
@@ -56,3 +56,72 @@ test('object is not moved when out of bounds', () => {
   expect(obj.position.x).not.toBe(10);
   expect(obj.position.y).not.toBe(10);
 });
+
+
+test('object grabs another object by its ID', () => {
+  const world = GidgetWorld.create();
+  const obj = world.createObject();
+  const grabbedObj = world.createObject();
+
+  expect(world.objects.length).toBe(2);
+  expect(obj.grab(grabbedObj.id)).toBe(true);
+  expect(world.objects.length).toBe(1);
+  expect(obj.grabbed.length).toBe(1);
+});
+
+
+test('object drops another object', () => {
+  const world = GidgetWorld.create();
+  const obj = world.createObject();
+  const grabbedObj = world.createObject();
+
+  // grab the object
+  expect(obj.grab(grabbedObj.id)).toBe(true);
+  expect(world.objects.length).toBe(1);
+  expect(obj.grabbed.length).toBe(1);
+
+  // drop the object
+  expect(obj.drop(grabbedObj.id)).toBe(true);
+  expect(world.objects.length).toBe(2);
+  expect(obj.grabbed.length).toBe(0);
+});
+
+
+test('object drops itself', () => {
+  const world = GidgetWorld.create();
+  const obj = world.createObject();
+  const grabbedObj = world.createObject();
+
+  // Grab grabbedObj
+  expect(obj.grab(grabbedObj.id)).toBe(true);
+  expect(world.objects.length).toBe(1);
+
+  // grabbedObj drops itself
+  expect(obj.grabbed.length).toBe(1);
+  expect(grabbedObj.drop()).toBe(true);
+  expect(obj.grabbed.length).toBe(0);
+  expect(world.objects.length).toBe(2);
+});
+
+
+test('object is removed', () => {
+  const world = GidgetWorld.create({ size: 3 });
+  const obj = world.createObject();
+
+  expect(world.objects.length).toBe(1);
+  expect(obj.remove()).toBe(true);
+  expect(world.objects.length).toBe(0);
+});
+
+
+test('object is not removed when grabbed', () => {
+  const world = GidgetWorld.create({ size: 3 });
+  const obj = world.createObject();
+  const grabbedObj = world.createObject();
+  obj.grab(grabbedObj.id);
+
+  expect(world.objects.length).toBe(1);
+  expect(grabbedObj.remove()).toBe(false);
+  expect(obj.grabbed.length).toBe(1);
+  expect(world.objects.length).toBe(1);
+});
