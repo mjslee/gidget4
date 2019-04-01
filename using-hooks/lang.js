e14f0c0eb6cc5484fe329cc7089ec036f56e6e01
thetarkus <mike@mikemob.com>
2019-04-01 11:45:35 -0400

    Text injection

--- using-hooks/lang.js
+++ using-hooks/lang.js
@@ -1,74 +1,100 @@
 const JSWalker = {
-	steps: [],
+  steps: [],
+  __blockStatements: ['WhileStatement', 'IfStatement', 'ForStatement'],
 
-	traverse(node, callback, parentNode=undefined) {
-		if (node === undefined || node === null)
-			return;
+  traverse(node, callback, parentNode=undefined) {
+    if (node === undefined || node === null)
+      return;
 
-		const traverse = node => {
-			if (typeof callback === 'function')
-				callback.call(this, node, parentNode);
+    const traverse = node => {
+      if (typeof callback === 'function')
+        callback.call(this, node, parentNode);
 
-			this.traverse(node.body, callback, node);
-			this.traverse(node.consequent, callback, node);
-			this.traverse(node.alternate, callback, node);
-		};
+      this.traverse(node.body, callback, node);
+      this.traverse(node.consequent, callback, node);
+      this.traverse(node.alternate, callback, node);
+      this.traverse(node.cases, callback, node);
+    };
 
-		if (!Array.isArray(node))
-			traverse(node);
-		else
-			for (let i = 0, len = node.length; i < len; i++)
-				traverse(node[i]);
-	},
-
-
-	toTree(input) {
-		return esprima.parseScript(input, { range: true });
-	},
+    if (!Array.isArray(node))
+      traverse(node);
+    else
+      for (let i = 0, len = node.length; i < len; i++)
+        traverse(node[i]);
+  },
 
 
+  toTree(input) {
+    return esprima.parseScript(input, { range: true });
+  },
 
-	run(input) {
-		//const originalInput = input;
-		const ast = this.toTree(input);
 
-		const insertAt = (text, input, position) =>
-			text.substr(0, position + input.length) + input + input.substr(position);
-		
-		let textModifications = [];
-		this.traverse([ast], (node, nodeParent) => {
-			console.log(node.type, nodeParent ? nodeParent.type : '');
-			// textModifications.push({ type: node.type, position: node.range[0], start: true });
-			// textModifications.push({ type: node.type, position: node.range[1], end: true });
-		});
 
-		textModifications.sort((a, b) => b.position - a.position).forEach((action) => {
-			input = input.substr(0, action.position) + "{" + action.type + "}" + input.substr(action.position);
-		});
+  run(input) {
+    //const originalInput = input;
+    const ast = this.toTree(input);
 
-		// console.log(input);
+    let textModifications = [];
+    this.traverse([ast], (node, nodeParent) => {
+      console.log(node, nodeParent);
 
-		// window.input=input;
-		// console.log(input);
+      // Ignore these
+      if (node.type === 'SwitchCase' || node.type === 'BreakStatement')
+        return;
 
-		/*let result = '';
-		for (let i = 0, len = lines.length; i < len; i++)
-			result += this.inject(i, lines[i]);
+      // Add text around the inside of blocks
+      if (node.type === 'BlockStatement') {
+        textModifications.push(['X', node.range[0] + 1]);
+        textModifications.push(['Y', node.range[1] - 1]);
+      }
 
-		this.inputCode = input;
-		console.log(result);
+      // Add text around expressions
+      else {
+        textModifications.push(['A', node.range[0]]);
+        textModifications.push(['B', node.range[1]]);
+      }
 
-		(() => {
-			const window = undefined;
-			const document = undefined;
+      // Add blocks around statements
+      if (
+        nodeParent && this.__blockStatements.includes(nodeParent.type) &&
+        node.type !== 'BlockStatement'
+      ) {
+        // Add opening paren
+        textModifications.push(['{', node.range[0]]);
 
-			const __this = this;
-			const __startLine = this.startLine;
-			const __endLine = this.endLine;
+        // Add closing paren
+        textModifications.push(['}', node.range[1]]);
+      }
+    });
 
-			eval(result);
-		}).call();
-		*/
-	}
+    textModifications.sort((a, b) => b[1] - a[1]).forEach((mod) => {
+      input = input.substring(0, mod[1]) + mod[0] + input.substring(mod[1]);
+    });
+
+    console.log('');
+    console.log(input);
+
+    // window.input=input;
+    // console.log(input);
+
+    /*let result = '';
+    for (let i = 0, len = lines.length; i < len; i++)
+      result += this.inject(i, lines[i]);
+
+    this.inputCode = input;
+    console.log(result);
+
+    (() => {
+      const window = undefined;
+      const document = undefined;
+
+      const __this = this;
+      const __startLine = this.startLine;
+      const __endLine = this.endLine;
+
+      eval(result);
+    }).call();
+    */
+  }
 
 };
