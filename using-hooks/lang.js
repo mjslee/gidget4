f434decf232a339df9e2319f2fdcab9ce8fd7040
thetarkus <mike@mikemob.com>
2019-04-04 23:29:54 -0400

    Add individual step calling

--- using-hooks/lang.js
+++ using-hooks/lang.js
@@ -1,119 +1,3 @@
-const JSWalker = {
-
-  __steps: [],
-  __index: 0,
-
-  __blockStatements: ['WhileStatement', 'IfStatement', 'ForStatement'],
-
-  step() {
-    return this.__steps[this.__index++];
-  },
-
-
-  traverse(node, callback, parentNode=undefined) {
-    if (node === undefined || node === null)
-      return;
-
-    const traverse = node => {
-      if (typeof callback === 'function')
-        callback.call(this, node, parentNode);
-
-      this.traverse(node.body, callback, node);
-      this.traverse(node.consequent, callback, node);
-      this.traverse(node.alternate, callback, node);
-      this.traverse(node.cases, callback, node);
-    };
-
-    if (!Array.isArray(node))
-      traverse(node);
-    else
-      for (let i = 0, len = node.length; i < len; i++)
-        traverse(node[i]);
-  },
-
-
-  getIdentifiers(input) {
-    return esprima.tokenize(input, { loc: true, range: true })
-      .filter(token => token.type === 'Identifier')
-  },
-
-
-  inject(tree, input) {
-    let result = [];
-
-    const identifiers = this.getIdentifiers(input);
-    this.traverse([tree], (node, parentNode) => {
-      // Ignore these
-      if (node.type === 'SwitchCase' || node.type === 'BreakStatement')
-        return;
-
-      let ln = node.loc.start.line;  // Line number
-
-      // Add text around the inside of blocks
-      if (node.type === 'BlockStatement')
-        result.push([`__scope__(${parentNode.loc.start.line});`, node.range[0] + 1]);
-
-      // Add text around expressions
-      else {
-        let pairs = '';
-        identifiers.forEach(id => {
-          let value = id.value;
-          if (id.range[0] < node.range[1])
-            pairs += `'${value}':typeof ${value}!=='undefined'?${value}:undefined,`;
-        });
-
-        result.push([
-          `__expr__(${ln}, ${node.range.join()}, {${pairs}});`, node.range[1]
-        ]);
-      }
-
-      // Add blocks around statements
-      if (
-        parentNode && this.__blockStatements.includes(parentNode.type) &&
-        node.type !== 'BlockStatement'
-      ) {
-        // Add opening paren
-        result.push([`{__scope__(${parentNode.loc.start.line});`, node.range[0]]);
-
-        // Add closing paren
-        result.push(['}', node.range[1]]);
-      }
-    });
-
-    result.sort((a, b) => b[1] - a[1]).forEach((mod) => {
-      input = input.substring(0, mod[1]) + mod[0] + input.substring(mod[1]);
-    });
-    
-
-    return input;
-  },
-
-
-  run(input) {
-    let tree = esprima.parseScript(input, { range: true, loc: true });
-    let debugInput = this.inject(tree, input);
-
-    (() => {
-      const window = undefined;
-      const document = undefined;
-
-      const __scope__ = ln => this.__steps.push({ lineNumber: ln });
-      const __expr__ = (ln, from, to, vars) => {
-        this.__steps.push({
-          lineNumber: ln,
-          range: [from, to],
-          variables: vars
-        });
-      };
-
-      eval(debugInput);
-    }).apply({});
-  }
-
-};
-
-
-
 const Stepper = {
   steps: [],
   index: 0,
@@ -122,7 +6,15 @@ const Stepper = {
    *
    */
   step() {
-    return this.steps[this.index++];
+    if (this.index >= this.steps.length)
+      return undefined;
+
+    const step = this.steps[this.index++];
+    const command = step.command;
+    if (typeof command === 'object')
+      command.target[command.prop].apply(this, command.args);
+
+    return step;
   },
 
   /*
@@ -138,8 +30,23 @@ const Stepper = {
   /*
    *
    */
+  spy(obj) {
+    return new Proxy(obj, {
+      get: (target, prop) => {
+        return (...args) => {
+          this.tempCommand = { target, prop, args };
+        };
+      }
+    });
+  },
+
+
+  /*
+   *
+   */
   __step__(ln, range, data) {
-    console.log(ln, range, data);
+    this.steps.push({ ln, range, data, command: this.tempCommand });
+    this.tempCommand = undefined;
   },
 
 
@@ -147,7 +54,7 @@ const Stepper = {
    *
    */
   __scope__(ln, range) {
-    console.log(ln, range);
+    this.steps.push({ ln, range });
   },
 
    
@@ -159,18 +66,18 @@ const Stepper = {
 
     this.debugInput = Injector(input);
 
-    const __scope__ = this.__scope__;
-    const __step__ = this.__step__;
-
-
     const fakeSandbox = () => {
       const window = undefined;
       const document = undefined;
+      const print = this.spy(console);
+
+      const __scope__ = (...args) => this.__scope__(...args);
+      const __step__ = (...args) => this.__step__(...args);
 
       eval(this.debugInput);
     };
 
-    fakeSandbox.call();
+    fakeSandbox.call({});
   }
 };
 
@@ -267,8 +174,10 @@ const Injector = (input) => {
       let parentLn = parentNode ? parentNode.loc.start.line : '';
 
       // Add scope step to BlockStatements
-      if (node.type === 'BlockStatement')
-        result.push([`__scope__(${parentLn});`, node.range[0] + 1]);
+      if (node.type === 'BlockStatement') {
+        const range = '[' + parentNode.range.join() + ']';
+        result.push([`__scope__(${parentLn},${range});`, node.range[0] + 1]);
+      }
 
       // Add scope to node that /could/ have a block statement
       else if (parentNode && blockNodes.includes(parentNode.type)) {
@@ -305,5 +214,3 @@ const Injector = (input) => {
 
   return this.main();
 };
-
-
