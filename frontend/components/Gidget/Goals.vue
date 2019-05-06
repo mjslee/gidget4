2ca2e34a0f91b5b47aeafd2b9044747cd0bc4367
thetarkus <mike@mikemob.com>
2019-05-05 22:00:36 -0400

    Add argument formatting for gidget goals

--- frontend/components/Gidget/Goals.vue
+++ frontend/components/Gidget/Goals.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <div v-for="goal in gameGoals" :key="goal.id">
-      {{ goal }}
+    <div class="goals" v-for="goal in gameGoals" :key="goal.id">
+      <div v-if="goal.assert === 'equal'">
+        <span class="keyword">assert</span>
+        <span class="argument" v-html="formatArgument(goal.arguments[0])" />
+        <span class="operator">===</span>
+        <span class="argument" v-html="formatArgument(goal.arguments[0])" />
+      </div>
     </div>
     <button @click="validateGoals">Validate</button>
   </diV>
@@ -9,7 +14,12 @@
 
 
 <style>
+.goals {
+  font-family: monospace;
+}
 
+.keyword { color: #770088 }
+.operator { color: #221199 }
 </style>
 
 
@@ -40,6 +50,20 @@ export default {
 
   methods: {
     /**
+     *
+     */
+    formatArgument(arg) {
+      const args = arg.split('.');
+      if (args.length > 1) {
+        let result = `<span class="object">${args[0]}</span>.`;
+        result += `<span class="prop">${args[1]}</span>`;
+        return result;
+      }
+      else
+        return `<span class="variable">${arg}</span>`;
+    },
+
+    /**
      * Set data to use for validation.
      */
     setData(data) {
@@ -52,7 +76,7 @@ export default {
     validateGoals() {
       const goalValidator = Goal.create(this.world, this.data);
       this.gameGoals.forEach(goal => {
-        this.$set(goal, 'completed', goalValidator.validate(goal))
+        this.$set(goal, 'completed', goalValidator.validate(goal));
       });
     }
   }
