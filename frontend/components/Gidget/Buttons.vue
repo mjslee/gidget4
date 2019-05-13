a559f52571840e93c3f53ea3ece7ee9c46d835cb
thetarkus <mike@mikemob.com>
2019-05-12 22:54:31 -0400

    Start button styling and combine start/stop buttons

--- frontend/components/Gidget/Buttons.vue
+++ frontend/components/Gidget/Buttons.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>
-    <button ref="explain" @click="$emit('click:explain')" :disabled='isBusy'>Explain Step</button>
-    <button ref="step" @click="$emit('click:step')" :disabled='isBusy'>Next Step</button>
-    <button ref="run" @click="$emit('click:run')" :disabled="isRunning">Run</button>
-    <button ref="stop" @click="$emit('click:stop')">Stop</button>
+  <div class="buttons has-addons">
+    <!-- <button -->
+    <!--   ref="explain" -->
+    <!--   class="button" -->
+    <!--   :disabled='isBusy' -->
+    <!--   @click="$emit('click:explain')" -->
+    <!-- > -->
+    <!--   Explain Step -->
+    <!-- </button> -->
+
+    <button
+      ref="step"
+      class="button"
+      :disabled='isBusy'
+      @click="$emit('click:step')"
+    >
+      Next Step
+    </button>
+
+    <!-- <button -->
+    <!--   ref="run" -->
+    <!--   class="button" -->
+    <!--   :disabled="isRunning" -->
+    <!--   @click="$emit('click:run')" -->
+    <!-- > -->
+    <!--   Run -->
+    <!-- </button> -->
+
+    <!-- <button -->
+    <!--   ref="stop" -->
+    <!--   class="button" -->
+    <!--   @click="$emit('click:stop')" -->
+    <!-- > -->
+    <!--   Stop -->
+    <!-- </button> -->
+
+    <button
+      ref="toggle"
+      v-text="toggleText"
+      :class="toggleClass"
+      @click="toggle"
+    />
+
   </div>
 </template>
 
@@ -18,6 +56,19 @@ export default {
     }
   },
 
+
+  computed: {
+    toggleClass() {
+      return 'button ' + (this.isRunning ? 'is-danger' : 'is-success')
+    },
+
+
+    toggleText() {
+      return this.isRunning ? 'Stop' : 'Run'
+    }
+  },
+
+
   methods: {
     /**
      *
@@ -25,6 +76,16 @@ export default {
     reset() {
       this.isRunning = false;
       this.isBusy = false;
+    },
+
+    toggle() {
+      // Not running? Run it
+      if (!this.isRunning)
+        this.$emit('click:run');
+
+      // It is running so stop it
+      else
+        this.$emit('click:stop');
     }
   }
 }
