1a3f5305f72799b2b5f742b8f1b3d26128237c6a
thetarkus <mike@mikemob.com>
2019-05-18 00:16:46 -0400

    Correctly enable next step button when appropriate

--- frontend/components/Gidget/Controls.vue
+++ frontend/components/Gidget/Controls.vue
@@ -26,7 +26,7 @@
       <b-button
         ref="nextStep"
         class="button"
-        :disabled='isBusy || stepIndex > stepCount'
+        :disabled='isBusy || (stepCount > 0 && stepIndex >= stepCount)'
         @click="$emit('change:step', ++stepIndex)"
       >
         Next Step
