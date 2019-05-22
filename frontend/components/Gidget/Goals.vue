e16a5a4643c01a5ec749e68e1fdf5fe0df44281c
thetarkus <mike@mikemob.com>
2019-05-22 08:48:25 -0400

    Remove formatArgument in favor of GidgetValue

--- frontend/components/Gidget/Goals.vue
+++ frontend/components/Gidget/Goals.vue
@@ -47,18 +47,20 @@ export default {
     GidgetValue
   },
 
+
   props: {
     goals: Array[Object],
     world: Object
   },
 
+
   data() {
     return {
-      objects: undefined,
       data: undefined,
       gameGoals: this.goals,
     };
   },
+
 
   created() {
     // Assign IDs to goals
@@ -68,35 +70,8 @@ export default {
     });
   },
 
+
   methods: {
-    /**
-     * Returns HTML of arguments wrapped in span elements.
-     */
-    formatArgument(arg) {
-      const span = (c, v) => `<span class="${c}">${v}</span>`;
-      const type = typeof arg;
-
-      if (type === 'string') {
-        // Is argument of form: 'Object.property'?
-        if (arg.includes('.')) {
-          const args = arg.split('.');
-          if (args.length > 1)
-            return span('object', args[0]) + '.' + span('property', args[1]);
-        }
-
-        // Argument starts with single or double quote -> string literal
-        if (arg[0] === '\'' || arg[0] === '"')
-          return span('string', arg);
-      }
-
-      // Argument is a number literal
-      if (type === 'number')
-        return span('literal', arg);
-
-      // Any other type of argument
-      return span('variable', arg);
-    },
-
     /**
      * Set data to use for validation.
      */
