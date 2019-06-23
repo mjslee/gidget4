596f791670dc866088dcb827d5a3b14b32f11c6e
thetarkus <mike@mikemob.com>
2019-06-23 17:25:56 -0400

    :heartpulse: Add support for new lines and block code in GidgetText

--- frontend/components/Gidget/Text.vue
+++ frontend/components/Gidget/Text.vue
@@ -2,8 +2,10 @@
   <span>
     <template v-for="(value, i) in internalText">
       <template v-if="typeof value === 'string'">{{ value }}</template>
-      <GidgetValue :literal="value[0]" :key="i" v-else-if="value[1] === 0" />
-      <highlight-code inline lang="javascript" :key="i" v-else-if="value[1] === 1">{{ value[0] }}</highlight-code>
+      <br v-else-if="value[1] === 0" />
+      <GidgetValue :literal="value[0]" :key="i" v-else-if="value[1] === 1" />
+      <highlight-code lang="javascript" :key="i" v-else-if="value[1] === 2">{{ value[0] }}</highlight-code>
+      <highlight-code inline lang="javascript" :key="i" v-else-if="value[1] === 3">{{ value[0] }}</highlight-code>
     </template>
   </span>
 </template>
@@ -11,6 +13,7 @@
 
 <script>
 import GidgetValue from './Value'
+
 
 export default {
   components: {
@@ -32,20 +35,28 @@ export default {
      */
     internalText() {
       // Split text by backtick or forward slash surroundings
-      const splits = this.text.split(/(\/\w+\/|`.*?`)/g)
+      const splits = this.text.split(/(\/\w+\/|```.*?```|`.*?`|\n)/g)
 
       // Loop over each element of the split text
       for (var i = splits.length - 1; i >= 0; i--) {
         // Type -1 will be ignored
         const type = -1;
 
-        // Type 0 is a forward slash; wrap with GidgetValue
-        if (this.isCharacterSurrounding(splits[i], '/'))
+        // Type 0 is a new character line \n
+        if (splits[i] === '\n')
           type = 0;
 
-        // Type 1 is a backtick; wrap with vue-highlightjs
-        else if (this.isCharacterSurrounding(splits[i], '`'))
+        // Type 1 is a forward slash; wrap with GidgetValue
+        else if (this.isSurrounded(splits[i], '/'))
           type = 1;
+
+        // Type 2 is a triple backtick; wrap with vue-highlightjs
+        else if (this.isSurrounded(splits[i], '```'))
+          type = 2;
+
+        // Type 3 is a backtick; wrap with vue-highlightjs inline
+        else if (this.isSurrounded(splits[i], '`'))
+          type = 3;
 
         // Replace element with object that contains two of its own elements:
         // the string element with first and last characters removed, and type
@@ -64,8 +75,8 @@ export default {
      * @param {string} text
      * @param {string} char
      */
-    isCharacterSurrounding(text, char) {
-      return text.charAt(0) === char && text.charAt(text.length - 1) === char;
+    isSurrounded(text, char) {
+      return text.startsWith(char) && text.endsWith(char);
     }
   }
 }
