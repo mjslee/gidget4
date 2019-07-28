84dc7ded90a3ea58f1d192fc1e2e6603a6057473
RichardFebres <rf57@njit.edu>
2019-07-27 22:00:03 -0400

    WIP: Create landing page

---
+++ frontend/layouts/landing.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div class="block">
+      <b-button icon-left="facebook"></b-button>
+      <b-button icon-left="twitter"></b-button>
+    </div>
+
+    <div class="block">
+      <b-dropdown hoverable aria-role="menu">
+        <button class="button is-info" slot="trigger">Account Name</button>
+      <b-dropdown-item aria-role="menuitem">Change Name</b-dropdown-item>
+      <b-dropdown-item aria-role="menuitem">Change Password</b-dropdown-item> 
+      <b-dropdown-item aria-role="menuitem">Logout</b-dropdown-item> 
+
+      </b-dropdown-item>
+      </b-dropdown>
+    </div>
+
+    <nuxt />
+  </main>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Landing',
+          icon: 'home',
+          to: { name: 'landing' }
+        }
+      ]
+    }
+  }
+}
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,44 +1,63 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
+  <div>
+    <section class="hero">
+      <div class="hero-body is-inline">
+        <div class="container">
+          <h1 class="title">Gidget</h1>
+        </div>
+      </div>
+    </section>
 
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
-      </card>
+    <section class="section">
+      <div class="container">
+          <div class="columns">
+            <div class="column is-three-quarters">
+              <div class="gidget-and-friends" alt="Gidget and friends" />
+              <!-- <img src="gidget/intro/landingDefault.png" alt="Gidget and friends" /> -->
+            </div>
+            <div class="column">
+              <b-button size="is-large" class="is-fullwidth">Play</b-button>
+              <b-button size="is-large" class="is-fullwidth" disabled>Level Editor</b-button>
+              <b-button size="is-large" class="is-fullwidth">About</b-button>
+            </div>
+          </div>
+      </div>
+    </section>
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-
-    </div>
-  </section>
+  </div>
 </template>
+
+
+<style scoped>
+html, body {
+  background: #82af55;
+}
+
+.hero {
+  background: #79cbe8;
+}
+
+.gidget-and-friends {
+  background: url('/gidget/intro/landingDefault.png') no-repeat;
+  height: 436px;
+}
+
+.gidget-and-friends:hover {
+  background-image: url('/gidget/intro/landingHover.png');
+}
+
+section {
+  background: url('')
+}
+</style>
+
 
 <script>
 import Card from '~/components/Card'
 
 export default {
   name: 'HomePage',
+  layout: 'landing',
 
   components: {
     Card
