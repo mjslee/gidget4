8bebb74bfe97d26c1afdf846ea6b8805c07c3a2a
root <root@LAPTOP-11BVG67C.localdomain>
2019-07-31 21:17:29 -0400

    Level Title Code

---
+++ frontend/components/Gidget/Title.vue
@@ -0,0 +1,28 @@
+<template>
+    
+<div id="titleBar"> 
+<div id="titleBarLeft"> 
+<div id="levelTitle">Level 1. Let's get to the puppy!</div> 
+<div id="levelProgress">
+<div id="level-marker-1" class="level-marker level-passed" title="Level 1: Let's get to the puppy!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-2" class="level-marker level-passed" title="Level 2: Let's go grab the piglet!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-3" class="level-marker level-passed" title="Level 3: Let's drop things into the correct containers!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-4" class="level-marker level-passed" title="Level 4: Let's organize everything correctly!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-5" class="level-marker level-passed" title="Level 5: Let's save the kitten twins!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-6" class="level-marker level-passed level-is-test" title="Level 6: New Level 2"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-7" class="level-marker level-passed level-is-test" title="Level 7: Moving Gidget"><i class="fa fa-circle fa-fw level-marker-image"></i></div><span class="level-unit-boundary"><i class="fa fa-ellipsis-v fa-fw level-marker-image"></i></span>
+<div id="level-marker-8" class="level-marker level-passed" title="Level 8: Where Will Gidget End Up?"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-9" class="level-marker level-passed" title="Level 9: Where Will the Goop End Up?"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-10" class="level-marker level-passed" title="Level 10: Let's move more efficiently to each object!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-11" class="level-marker level-passed" title="Level 11: Let's move around more efficiently to specific spots on the map!"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-12" class="level-marker level-passed level-is-test" title="Level 12: Testing Gidget's Mobility with Goto"><i class="fa fa-circle fa-fw level-marker-image"></i></div>
+<div id="level-marker-13" class="level-marker level-passed level-is-test" title="Level 13: Let's organize two of a kind!"><i class="fa fa-circle fa-fw level-marker-image"></i></div><span class="level-unit-boundary"><i class="fa fa-ellipsis-v fa-fw level-marker-image"></i></span>
+</div> 
+    <div id="levelDesignHead" style="display: none;"> 
+        <div id="levelDesignLevelTitle" style="display: none;"> 
+<span>Title:</span> <input type="text" id="levelDesignLevelTitleField"> 
+        </div> 
+    </div> 
+</div> 
+
+</template>
