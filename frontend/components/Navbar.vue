10344e51ad71386fa20c75444f53714f4dda5ae0
thetarkus <mike@mikemob.com>
2019-09-25 13:19:47 -0400

    Add user store template

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-navbar>
+      <template slot="start"></template>
+
+      <template slot="end">
+        <b-navbar-item>Login</b-navbar-item> 
+      </template>
+
+    </b-navbar>
+  </div>
+</template>
