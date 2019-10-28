40aefa7cb7b58f50cd8ce11e9c7b8a98697f2cf8
thetarkus <mike@mikemob.com>
2019-10-28 01:03:28 -0400

    Add 'Login' vue component

---
+++ frontend/components/Auth/Login.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <section>
+
+    <b-field>
+      <b-input placeholder="E-mail" v-model="email" />
+    </b-field>
+
+    <b-field>
+      <b-input placeholder="Password" v-model="password" password-reveal />
+    </b-field>
+
+    <div class="field is-grouped is-grouped-right">
+      <b-button
+        type="is-primary"
+        size="is-medium"
+        icon-right="chevron-right"
+      >
+        Continue
+      </b-button>
+    </div>
+
+  </section>
+</template>
+
+
+<style scoped>
+.is-horizontal {
+  margin-bottom: 0;
+}
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
