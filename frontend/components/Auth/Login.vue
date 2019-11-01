41906aab7ac3b9caf7b778ce933f5208ef3c3e18
thetarkus <mike@mikemob.com>
2019-11-01 01:41:43 -0400

    Add log in submit form, loading spinner

--- frontend/components/Auth/Login.vue
+++ frontend/components/Auth/Login.vue
@@ -6,8 +6,14 @@
     </button>
 
     <!-- Modal -->
-    <b-modal :active.sync="isModalActive" :width="640">
-      <div class="modal-card" style="width: auto">
+    <b-modal :active.sync="isModalActive" :width="400">
+
+        <b-loading
+          :is-full-page="false"
+          :active.sync="isLoading"
+          :can-cancel="true"
+        />
+      <form @submit.prevent="submitForm" class="modal-card" style="width: auto">
 
         <!-- Modal Header -->
         <header class="modal-card-head">
@@ -16,32 +22,46 @@
 
         <!-- Modal Body -->
         <section class="modal-card-body">
+          <b-notification
+            type="is-danger"
+            v-for="(value, key) in formErrors"
+            :key="key"
+          >
+            <p v-for="error in value" :key="error">{{ error }}</p>
+          </b-notification>
+
           <b-field label="E-mail" expanded>
-            <b-input placeholder="E-mail" v-model="formData.email" />
+            <b-input
+              v-model="formData.email"
+              type="email"
+              placeholder="E-mail"
+              required
+            />
           </b-field>
 
           <b-field label="Password" expanded>
             <b-input
+              v-model="formData.password"
               type="password"
               placeholder="Password"
-              v-model="formData.password"
               password-reveal
-              />
+              required
+            />
           </b-field>
         </section>
 
         <!-- Modal Footer -->
         <section class="modal-card-foot">
           <b-button
-            @click="submit"
             type="is-primary"
+            native-type="submit"
             icon-right="chevron-right"
           >
             Continue
           </b-button>
         </section>
 
-      </div>
+      </form>
     </b-modal>
 
   </section>
@@ -62,16 +82,37 @@ import { LOGIN_ENDPOINT } from '@/constants/endpoints'
 export default {
   data: () => ({
     isModalActive: false,
+    isLoading: false,
 
+    formErrors: {},
     formData: {
-      email: '',
-      password: ''
+      email: 'a@a.com',
+      password: 'a'
     }
   }),
 
   methods: {
-    submit() {
-      /* axios call */
+    async submitForm() {
+      this.isLoading = true
+
+      // Fetch response from signup endpoint
+      try {
+        const response = await this.$axios.post(LOGIN_ENDPOINT, this.formData)
+      }
+
+      // Set form errors
+      catch (err) {
+        const formErrors = _.get(err, 'response.data.errors')
+        console.log(formErrors);
+
+        if (typeof formErrors != 'undefined')
+          this.formErrors = formErrors
+      }
+
+      // Not loading anymore
+      finally {
+        this.isLoading = false
+      }
     }
   }
 }
