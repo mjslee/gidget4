34a5c7c83c57442955c50401ff7f6d4683d143b2
thetarkus <mike@mikemob.com>
2019-11-11 00:47:58 -0500

    Add level sets table migration

---
+++ backend/database/migrations/2019_11_11_052143_create_level_sets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLevelSetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('level_sets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('published')->default(false);
+            $table->boolean('official')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('level_sets');
+    }
+}
