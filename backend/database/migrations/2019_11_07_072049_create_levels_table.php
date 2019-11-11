c9ba8209ebd1a5b3a8d26f8786c1c96bf572169f
thetarkus <mike@mikemob.com>
2019-11-11 14:34:10 -0500

    Add create levels table migration

--- backend/database/migrations/2019_11_07_072049_create_levels_table.php
+++ backend/database/migrations/2019_11_07_072049_create_levels_table.php
@@ -13,7 +13,7 @@ class CreateLevelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('levels', function (Blueprint $table) {
+       Schema::create('levels', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned()->nullable();
             $table->string('name');
