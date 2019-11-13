36f36b002f0a52cd44b536f09130daf9e95ab816
thetarkus <mike@mikemob.com>
2019-11-12 23:27:14 -0500

    Remove has_coding_experience as a fillable field

--- backend/app/Models/UserProfile.php
+++ backend/app/Models/UserProfile.php
@@ -4,16 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class UserProfile extends Model
 {
-    protected $hidden = [
-        'id', 'user_id', 'created_at', 'updated_at'
-    ];
-
-    protected $fillable = [
-        'name',
-        'gender',
-        'birthday',
-        'has_coding_experience'
-    ];
+    protected $hidden = [ 'id', 'user_id', 'created_at', 'updated_at' ];
+    protected $fillable = [ 'name', 'gender', 'birthday' ];
 }
