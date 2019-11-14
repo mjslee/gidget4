67509ab8825f79088c2d56eed8f7301e36972d90
thetarkus <mike@mikemob.com>
2019-11-13 20:59:02 -0500

    Add API register view to RegisterController

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -1,12 +1,16 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\RegistersUsers;
+
 
 class RegisterController extends Controller
 {
@@ -49,9 +53,15 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => [
+                'required', 'string', 'max:255'
+            ],
+            'email' => [
+                'required', 'string', 'email', 'max:255', 'unique:users'
+            ],
+            'password' => [
+                'required', 'string', 'min:8', 'confirmed'
+            ],
         ]);
     }
 
@@ -59,7 +69,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
@@ -68,5 +78,37 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * User registration attempt.
+     * 
+     * @param  Illuminate\Http\Request  $request
+     */
+    public function register(Request $request)
+    {
+        // POST data
+        $data = $request->all();
+
+        // Validate request and create user
+        $this->validator($data)->validate();
+
+        // Create user
+        $user = $this->create($data);
+        event(new Registered($user));
+
+        return $this->registered($request, $user);
+    }
+
+
+    /**
+     * User has been registered.
+     *
+     * @param  Illuminate\Http\Request  $request
+     * @return \App\User
+     */
+    protected function registered(Request $request, User $user)
+    {
+        return response()->json(['message' => 'Successfully registered.'], 201);
     }
 }
