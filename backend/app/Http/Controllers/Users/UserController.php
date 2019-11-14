78409867f08e7412ed498585c57946b4f17645cf
thetarkus <mike@mikemob.com>
2019-11-14 02:18:10 -0500

    Add user update controller view (WIP)

--- backend/app/Http/Controllers/Users/UserController.php
+++ backend/app/Http/Controllers/Users/UserController.php
@@ -13,11 +13,45 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return [json] user object
      */
-    public function user(Request $request)
+    public function show(Request $request)
     {
         $user = $request->user();
         $user->profile = $user->profile;  // Fetch profile relation
 
         return response()->json($user);
+    }
+
+    /**
+     * Update user and/or user profile.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return [json] message
+     */
+    public function update(Request $request)
+    {
+        $data = $request->validate([
+            // User Fields
+            'name' => 'sometimes|required|max:255',
+
+            // User Profile Fields
+            'gender' => 'sometimes|required|max:255',
+            'birthdate' => 'sometimes|required|date'
+        ]);
+        
+        $user = $request->user();
+
+        // Update user
+        if (isset($user->name)) {
+            $user->name = $request->name;
+            $user->save();
+        }
+
+        // Update user profile
+        $user->profile()->updateOrCreate([], [
+            'gender' => $request->gender,
+            'birthdate' => $request->birthdate,
+        ]);
+
+        return response()->json(['message' => 'User updated.']);
     }
 }
