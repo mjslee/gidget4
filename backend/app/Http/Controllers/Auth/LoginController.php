4d55a3ea1d5ca3cc8f2aed329d777bce777925af
thetarkus <mike@mikemob.com>
2019-11-13 20:58:50 -0500

    Add API login view to LoginController

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -1,12 +1,16 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
 
 class LoginController extends Controller
 {
+
     /*
     |--------------------------------------------------------------------------
     | Login Controller
@@ -36,4 +40,33 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Login user and create token
+     *
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (!$this->attemptLogin($request))
+            return $this->sendFailedLoginResponse($request);
+
+        // Get new user
+        $user = $this->guard()->user();
+
+        // Create token
+        $token = $user->createToken('Personal Access Token');
+        $token->token->save();
+
+        return response()->json([
+            'access_token' => $token->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $token->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
+
 }
