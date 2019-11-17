61439343238f39721dbb53e9f1cd5f5ca5ded545
thetarkus <mike@mikemob.com>
2019-11-17 18:43:14 -0500

    UserProfile: Add birthdate to $fillables

--- backend/app/Models/UserProfile.php
+++ backend/app/Models/UserProfile.php
@@ -1,19 +1,36 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UserProfile extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'gender', 'birthdate', 'has_coding_experience'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
         'id', 'user_id', 'created_at', 'updated_at'
     ];
 
-    protected $fillable = [
-        'name',
-        'gender',
-        'birthday',
-        'has_coding_experience'
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'birthdate' => 'date:Y-m-d',
     ];
+
 }
