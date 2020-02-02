eaed6f8bb09d3a63673b26be06d5dd56155948c2
thetarkus <mike@mikemob.com>
2020-02-02 01:37:32 -0500

    LevelActivity: Add model and migrations

---
+++ backend/database/migrations/2020_01_28_052259_create_level_progress_table.php
@@ -0,0 +1,57 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateLevelProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('level_progress', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable(true);
+            $table->unsignedBigInteger('level_id')->nullable(true);
+
+            $table->json('data')->nullable(true);
+
+            $table->unsignedInteger('load_count')->default(0);
+            $table->unsignedInteger('update_count')->default(0);
+            $table->unsignedInteger('doc_count')->default(0);
+            $table->unsignedInteger('restore_count')->default(0);
+            $table->unsignedInteger('reset_count')->default(0);
+
+            $table->unsignedBigInteger('play_duration')->default(0);
+            $table->unsignedBigInteger('doc_duration')->default(0);
+            $table->unsignedBigInteger('focus_duration')->default(0);
+            $table->unsignedBigInteger('idle_duration')->default(0);
+            $table->unsignedBigInteger('tutorial_duration')->default(0);
+
+            $table->datetime('completed_at')->nullable(true);
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('level_id')->references('id')->on('levels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('level_progress', function($table) {
+            $table->dropForeign('level_progress_user_id_foreign');
+            $table->dropForeign('level_progress_level_id_foreign');
+        });
+
+        Schema::dropIfExists('level_progress');
+    }
+}
