c7af40f66c23896e9c5868449d952b19497a1990
thetarkus <mike@mikemob.com>
2020-02-04 16:16:48 -0500

    CodeHelper: Add hashCode method

--- backend/app/Helpers/CodeHelper.php
+++ backend/app/Helpers/CodeHelper.php
@@ -15,4 +15,15 @@ class CodeHelper
         return preg_replace('/[\s;\r\n]+/', '', $code);
     }
 
+    /**
+     * Create a SHA-1 hash of minified code.
+     *
+     * @param String $minifiedCode Code that has been minified.
+     * @return String Hash of the parameter.
+     */
+    public static function hashCode(String $minifiedCode) : String
+    {
+        return sha1($minifiedCode);
+    }
+    
 }
