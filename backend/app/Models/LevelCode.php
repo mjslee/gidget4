c6fd3ff6eb3abf112de276d4387fd975d0c1e59f
thetarkus <mike@mikemob.com>
2020-02-04 16:22:07 -0500

    LevelCode: Add model (WIP)

--- backend/app/Models/LevelCode.php
+++ backend/app/Models/LevelCode.php
@@ -2,9 +2,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\LevelProgress;
 
 
 class LevelCode extends Model
 {
-    //
+    /**
+     * @var Table name of LevelCode model.
+     */
+    protected $table = 'level_code';
+
+    public static function createInstance(LevelProgress $levelProgress, String $code) : LevelCode
+    {
+        $obj = new LevelCode;
+        $obj->progress()->associate($levelProgress);
+
+        return $obj;
+    }
+
+    public function progress()
+    {
+        return $this->belongsTo(LevelProgress::class);
+    }
 }
