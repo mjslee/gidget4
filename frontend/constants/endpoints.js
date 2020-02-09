2fe18f83ff5f1d1ae537ca5801b1dbf1a9ac830b
thetarkus <mike@mikemob.com>
2020-02-08 22:57:22 -0500

    Frontend: Add levels endpoint constant

--- frontend/constants/endpoints.js
+++ frontend/constants/endpoints.js
@@ -3,3 +3,6 @@ export const REGISTER_ENDPOINT = '/api/auth/register'
 export const LOGIN_ENDPOINT    = '/api/auth/login'
 export const LOGOUT_ENDPOINT   = '/api/auth/logout'
 export const USER_ENDPOINT     = '/api/user'
+
+// Levels
+export const LEVELS            = '/api/levels';
