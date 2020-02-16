9782033314307c84df828ee0a2e844bd24267694
thetarkus <mike@mikemob.com>
2020-02-16 00:51:07 -0500

    Frontend: Add levels page

---
+++ frontend/pages/levels.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <LevelTable />
+  </div>
+</template>
+
+
+<script>
+import LevelTable from '@/components/Levels/LevelTable';
+
+
+export default {
+
+  components: {
+    LevelTable
+  },
+
+  props: {
+
+  },
+
+  data() {
+    return { };
+  }
+}
+</script>
