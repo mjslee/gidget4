d286becea977c991c807b71deaf009847ef67f02
thetarkus <mike@mikemob.com>
2020-02-16 00:50:55 -0500

    Frontend: Add LevelTable component

---
+++ frontend/components/Levels/LevelTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-table
+      :columns="columns"
+      :data="rows"
+    ></b-table>
+  </div>
+</template>
+
+
+<script>
+import { LEVELS } from '@/constants/endpoints';
+
+
+export default {
+
+  props: {
+
+  },
+
+  data() {
+    return {
+      columns: [
+        {
+          field: 'id'
+        },
+        {
+          field: 'title'
+        },
+        {
+          field: 'description'
+        }
+      ],
+      rows: [],
+    };
+  },
+
+  async mounted() {
+    this.rows = await this.getLevels();
+  },
+
+  methods: {
+    async getLevels() {
+      const res = await this.$axios.$get(LEVELS);
+      return res.data;
+    }
+  },
+}
+</script>
