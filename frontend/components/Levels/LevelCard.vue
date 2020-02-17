0329f7a5aa20ff8a7b3cf633fba99496185a23e6
thetarkus <mike@mikemob.com>
2020-02-16 22:07:11 -0500

    Frontend: Add LevelCard component

---
+++ frontend/components/Levels/LevelCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+
+    <section class="card-content">
+      <p class="title">
+        {{ title }}
+      </p>
+      <p class="subtitle">
+        {{ description }}
+      </p>
+    </section>
+
+    <footer class="card-footer">
+      <nuxt-link :to="`/play/${id}`" class="card-footer-item">
+        Play
+      </nuxt-link>
+    </footer>
+
+  </div>
+</template>
+
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.card-footer {
+  margin-top: auto;
+}
+</style>
+
+
+<script>
+export default {
+  props: {
+    id: Number,
+    title: String,
+    description: String
+  }
+}
+</script>
