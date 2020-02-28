abc73e6b79753cbd4ced375cb3345e8243028fd2
thetarkus <mike@mikemob.com>
2020-02-27 23:00:02 -0500

    Add ProgressController

---
+++ backend/app/Http/Controllers/Levels/ProgressController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers\Levels;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class ProgressController extends Controller
+{
+
+    /**
+     * Update progress with newest activity time.
+     *
+     * @param \Illuminate\Http\Request $request [TODO:description]
+     * @return [TODO:type] [TODO:description]
+     */
+    function ping(Request $request)
+    {
+
+    }
+    
+}
