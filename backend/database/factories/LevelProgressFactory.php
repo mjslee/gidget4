c5dfb691c61660dac07822c26aa600c40ce6c818
thetarkus <mike@mikemob.com>
2020-02-28 20:49:38 -0500

    Add LevelProgress factory

---
+++ backend/database/factories/LevelProgressFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\LevelProgress;
+use Faker\Generator as Faker;
+
+$factory->define(LevelProgress::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
