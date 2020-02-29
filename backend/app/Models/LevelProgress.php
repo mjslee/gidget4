c4e6de2150519d60cc4368f7c210ac0e860f0622
thetarkus <mike@mikemob.com>
2020-02-28 19:15:38 -0500

    Add random string_id generation to LevelProgress::createInstance

--- backend/app/Models/LevelProgress.php
+++ backend/app/Models/LevelProgress.php
@@ -2,6 +2,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 use App\Models\User;
 use App\Models\Level;
@@ -22,11 +23,18 @@ class LevelProgress extends Model
      * @param \App\Models\User $user (optional) A user object.
      * @return LevelProgress Instance of LevelProgress.
      */
-    public static function createInstance(Level $level, User $user = null) : LevelProgress
+    public static function createInstance(Level $level, User $user = null): LevelProgress
     {
         $obj = new LevelProgress;
         $obj->level()->associate($level);
         $obj->user()->associate($user);
+
+        if (is_null($user)) {
+            do {
+                $obj->string_id = Str::random(64);
+            }
+            while (self::where('string_id', '=', $obj->string_id)->exists());
+        }
 
         return $obj;
     }
