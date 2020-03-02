198f69c50445c349d8484faa91da2fd33347e0ba
tarkusdev <mike@tarkus.dev>
2020-03-01 22:30:50 -0500

    Add progress show, run, and complete routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 /**
  * Authentication
  */
-Route::group(['prefix' => 'auth'], function() {
+Route::group(['prefix' => 'auth'], function () {
     Route::post('register', 'Auth\RegisterController@register')->name('register');
     Route::post('login', 'Auth\LoginController@login')->name('login');
 });
@@ -25,7 +26,7 @@ Route::group(['prefix' => 'auth'], function() {
 /**
  * Users
  */
-Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
     Route::get('/', 'Users\UserController@show')->name('user.show');
     Route::patch('/', 'Users\UserController@update')->name('user.update');
 });
@@ -34,7 +35,7 @@ Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
 /**
  * Levels
  */
-Route::group(['prefix' => 'levels'], function() {
+Route::group(['prefix' => 'levels'], function () {
     Route::get('/', 'Levels\LevelController@index')
         ->name('level.index');
 
@@ -48,4 +49,13 @@ Route::group(['prefix' => 'levels'], function() {
 
     Route::get('/{level}', 'Levels\LevelController@show')
         ->name('level.show');
+
+    Route::get('/{level}/progress', 'Levels\ProgressController@show')
+        ->name('progress.show');
+
+    Route::post('/{level}/progress', 'Levels\ProgressController@run')
+        ->name('progress.run');
+
+    Route::post('/{level}/progress/complete', 'Levels\ProgressController@complete')
+        ->name('progress.complete');
 });
