6025bfb05d23616ad5b4732fdd6f18b72bea8617
tarkusdev <mike@tarkus.dev>
2020-03-07 22:00:42 -0500

    Create array for LevelCollectionResource result

--- backend/app/Http/Resources/LevelCollectionResource.php
+++ backend/app/Http/Resources/LevelCollectionResource.php
@@ -14,6 +14,11 @@ class LevelCollectionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'          => $this->id,
+            'title'       => $this->title,
+            'description' => $this->description,
+            'levels'      => $this->levels()->paginate(5)
+        ];
     }
 }
