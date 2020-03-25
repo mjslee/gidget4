3d84c62b9c222ddbec607b96820e2bd4bb9871e8
thetarkus <mike@mikemob.com>
2020-03-25 00:58:01 -0400

    (WIP) Game Editor

---
+++ frontend/components/Gidget/Editor/GameEditor.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-tabs v-model="activeTab">
+      <b-tab-item label="Objects">
+        <object-list />
+      </b-tab-item>
+      <b-tab-item label="Dialogue">
+        Lorem ipsum dolor sit amet.
+      </b-tab-item>
+      <b-tab-item label="Goals">
+        Lorem ipsum dolor sit amet.
+      </b-tab-item>
+    </b-tabs>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
+
+
+<script>
+import ObjectList from '@/components/Gidget/Editor/ObjectList';
+
+
+export default {
+  components: {
+    ObjectList
+  },
+
+  data() {
+    return {
+      activeTab: 0
+    }
+  }
+}
+</script>
