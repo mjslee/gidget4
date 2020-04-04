4fc4707621d736c3869fd706f649baabb22591ef
thetarkus <mike@mikemob.com>
2020-04-03 23:25:55 -0400

    Add OrderButtons component

---
+++ frontend/components/Gidget/Utilities/OrderButtons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="buttons">
+    <b-button icon-right="chevron-up" :disabled="disabled" @click="$emit('up')"></b-button>
+    <b-button icon-right="chevron-down" :disabled="disabled" @click="$emit('down')"></b-button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    disabled: Boolean
+  }
+}
+</script>
