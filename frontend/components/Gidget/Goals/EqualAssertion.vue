84e15e0f12527e7487233ff882b266b817d09791
thetarkus <mike@mikemob.com>
2020-04-03 23:13:04 -0400

    Move Goals into its own dir

---
+++ frontend/components/Gidget/Goals/EqualAssertion.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span class="is-keyword">assert</span>
+    <GidgetValue :code="args[0]" />
+    <span class="is-operator">==</span>
+    <GidgetValue :code="args[1]" />
+  </div>
+</template>
+
+
+<script>
+import GidgetValue from '../Value'
+
+export default {
+  components: {
+    GidgetValue
+  },
+
+  props: {
+    args: Array
+  }
+};
+</script>
---
+++ frontend/components/Gidget/Goals/Goals.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="goals" v-for="(goal, i) in goals" :key="`goal-${i}`">
+      <b-icon
+        icon="close-circle"
+        type="is-danger"
+        size="is-small"
+        v-if="reveal && goal.isComplete === false"
+      />
+      <b-icon
+        icon="check-circle"
+        type="is-success"
+        size="is-small"
+        v-else-if="reveal && goal.isComplete"
+      />
+      <b-icon
+        icon="checkbox-blank-circle-outline"
+        size="is-small"
+        v-else
+      />
+      <span v-if="goal.assertion === 'equal'">
+        <EqualAssertion v-bind="{ args: goal.arguments }" />
+      </span>
+    </div>
+
+    <button @click="$emit('validate')">Validate</button>
+  </div>
+</template>
+
+
+<style>
+.goals {
+  font-family: monospace;
+}
+</style>
+
+
+<script>
+import GidgetValue from '../Value'
+import EqualAssertion from './EqualAssertion';
+
+
+export default {
+  components: {
+    GidgetValue,
+    EqualAssertion
+  },
+
+
+  data() {
+    return {
+      reveal: true
+    };
+  },
+
+
+  computed: {
+    goals() {
+      return this.$store.getters['goals/getGoals'];
+    }
+  },
+
+
+  methods: {
+    /**
+     * Reset goal completed status.
+     *
+     * @return {void}
+     */
+    reset() {
+      //this.reveal = false;
+    },
+
+
+    /**
+     * Validate all given goals.
+     *
+     * @return {void}
+     */
+    validate() {
+    },
+  }
+}
+</script>
