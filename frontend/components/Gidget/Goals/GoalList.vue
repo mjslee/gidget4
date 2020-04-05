37b01f27939d1ac245d56945dcc69e64bffe6681
thetarkus <mike@mikemob.com>
2020-04-05 01:29:40 -0400

    Add GoalList component
    This component shows all goals in the goal store for editing.

---
+++ frontend/components/Gidget/Goals/GoalList.vue
@@ -0,0 +1,42 @@
+<template>
+  <article>
+    <order-table :columns="columns" :data="goals">
+      <template slot="top-right-content">
+        <goal-create-button />
+      </template>
+      <template slot="row-details" slot-scope="props">
+
+      </template>
+    </order-table>
+  </article>
+</template>
+
+
+<script>
+import OrderTable from '../Utilities/OrderTable';
+import GoalCreateButton from './GoalCreateButton';
+
+
+export default {
+  components: {
+    OrderTable,
+    GoalCreateButton
+  },
+
+  computed: {
+    goals() {
+      return this.$store.getters['goals/getGoals'];
+    }
+  },
+
+  data() {
+    return {
+      columns: [
+        { field: 'id', label: 'ID' },
+        { field: 'assert', label: 'Assertion' },
+        { field: 'args', label: 'Arguments' },
+      ]
+    };
+  }
+}
+</script>
