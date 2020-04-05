24f5cd3a61721ef0690d3ec2ef472e0acb2e034d
thetarkus <mike@mikemob.com>
2020-04-05 01:28:40 -0400

    Add GoalCreateButton component

---
+++ frontend/components/Gidget/Goals/GoalCreateButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <article>
+    <b-button type="is-primary" @click="isModalActive = true">
+      New Goal
+    </b-button>
+
+    <portal to="modal">
+      <b-modal :active.sync="isModalActive" :width="640">
+        <div class="card">
+          <goal-form class="card-content" v-bind.sync="goal" @done="done">
+            <template slot="complete-button-text">Create</template>
+          </goal-form>
+        </div>
+      </b-modal>
+    </portal>
+
+  </article>
+</template>
+
+
+<script>
+import GoalForm from './GoalForm';
+import GoalArgumentInput from './GoalArgumentInput';
+
+
+export default {
+  components: {
+    GoalForm,
+    GoalArgumentInput
+  },
+
+  data() {
+    return {
+      isModalActive: true,
+      goal: {
+        assert: 'equals',
+        args: ['true', 'false']
+      }
+    };
+  },
+
+  methods: {
+    done() {
+      this.isModalActive = false;
+      this.$store.dispatch('goals/addGoal', this.goal);
+    }
+  }
+}
+</script>
