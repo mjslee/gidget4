67e3d6e66c051fde16aee82149383e2b8e7385fa
thetarkus <mike@mikemob.com>
2020-04-08 00:41:57 -0400

    Add switch button component

---
+++ frontend/components/Gidget/Utilities/SwitchButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="level-item">
+    <b-switch :type="type" v-model="canClick"></b-switch>
+    <b-button :type="type" @click="$emit('click')" :disabled="!canClick">
+      <slot>Remove</slot>
+    </b-button>
+  </article>
+</template>
+
+
+<script>
+export default {
+  props: {
+    type: String
+  },
+
+  data() {
+    return {
+      canClick: false
+    };
+  }
+}
+</script>
