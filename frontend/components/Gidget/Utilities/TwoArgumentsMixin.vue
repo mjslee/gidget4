b29f2a7ea6c192422a5cb5f7c3412f4cb7f8d3af
thetarkus <mike@mikemob.com>
2020-04-08 21:05:12 -0400

    Add TwoArgumentsMixin mixin

---
+++ frontend/components/Gidget/Utilities/TwoArgumentsMixin.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  computed: {
+    /**
+     * 0th argument.
+     *
+     * @param {string}
+     * @return {array[string]}
+     */
+    arg0: {
+      get() {
+        return this.value[0];
+      },
+      set(value) {
+        this.$emit('input', [value, this.arg1]);
+      }
+    },
+
+    /**
+     * 1st argument.
+     *
+     * @param {string}
+     * @return {array[string]}
+     */
+    arg1: {
+      get() {
+        return this.value[1];
+      },
+      set(value) {
+        this.$emit('input', [this.arg0, value]);
+      }
+    },
+  }
+}
+</script>
