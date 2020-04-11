cd836336502eece8945117baf8af2c10661b53b7
thetarkus <mike@mikemob.com>
2020-04-11 04:12:18 -0400

    Add WorldSettings component

---
+++ frontend/components/Gidget/Editor/WorldSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    <world-size-editor />
+  </article>
+</template>
+
+<script>
+import WorldSizeEditor from './WorldSizeEditor';
+
+export default {
+  components: {
+    WorldSizeEditor
+  }
+};
+</script>
