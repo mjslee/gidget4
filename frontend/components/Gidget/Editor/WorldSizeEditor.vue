aa808073318c1a18ddd05c05331de45ad2cf3ec3
thetarkus <mike@mikemob.com>
2020-04-11 04:08:28 -0400

    Add WorldSizeEditor component functionality

--- frontend/components/Gidget/Editor/WorldSizeEditor.vue
+++ frontend/components/Gidget/Editor/WorldSizeEditor.vue
@@ -1,20 +1,41 @@
 <template>
-  <div>
-    <input
-      :value="value" type="range" min="3" max="10"
-      @input="$emit('input', parseInt($event.target.value))"
-    />
-  </div>
+  <section>
+    <b-field grouped>
+      <b-field label="Height" expanded>
+        <b-slider v-model="worldSize.height" v-bind="sliderAttrs" />
+      </b-field>
+      <b-field label="Width" expanded>
+        <b-slider v-model="worldSize.width" v-bind="sliderAttrs" />
+      </b-field>
+    </b-field>
+  </section>
 </template>
 
 
 <script>
+import Vue from 'vue';
+
 export default {
-  props: {
-    value: {
-      type: Number,
-      default: 3
-    }
+  computed: {
+    worldSize: {
+      get() {
+        return this.$store.getters['game/getWorldSize'];
+      },
+      set({ height, width }) {
+        return this.$store.commit('game/setWorldSize', { height, width });
+      }
+    },
+  },
+
+  data() {
+    return {
+      sliderAttrs: {
+        rounded: true,
+        expand: true,
+        min: 3,
+        max: 10,
+      }
+    };
   }
 }
 </script>
