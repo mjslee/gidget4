188701a5f011ea08c7f140fbc63d0bbd33cdb0fa
thetarkus <mike@mikemob.com>
2020-04-11 04:12:09 -0400

    Add WorldEditor component

---
+++ frontend/components/Gidget/Editor/WorldEditor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="columns is-multi-line">
+    <div class="column">
+      <h1 class="title">World</h1>
+      <world />
+    </div>
+
+    <div class="column">
+      <div class="column">
+        <h1 class="title">Settings</h1>
+        <world-settings />
+      </div>
+      <div class="column" v-if="selectedObject">
+        <h1 class="title">Selected</h1>
+        <object-mover :object="selectedObject" />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import World from '../World';
+import WorldSettings from './WorldSettings';
+import ObjectMover from '../Objects/ObjectMover';
+import ObjectForm from '../Objects/ObjectForm';
+
+
+export default {
+  components: {
+    World,
+    WorldSettings,
+    ObjectMover,
+    ObjectForm
+  },
+
+  computed: {
+    selectedObject() {
+      return this.$store.getters['objects/getSelected'];
+    }
+  }
+};
+</script>
