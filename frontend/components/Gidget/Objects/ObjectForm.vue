5cdbba0945b53b7b6b44305ba5c4ec2bce82eb33
thetarkus <mike@mikemob.com>
2020-04-12 04:13:23 -0400

    ObjectForm: Add tabs for sprite selector/object settings

--- frontend/components/Gidget/Objects/ObjectForm.vue
+++ frontend/components/Gidget/Objects/ObjectForm.vue
@@ -1,49 +1,59 @@
 <template>
   <article class="media">
     <figure class="media-left">
-      <b-tooltip :label="sprite" position="is-bottom">
+      <b-tooltip :label="internalSprite" position="is-bottom" always>
         <div class="sprite image is-128x128">
-          <img :src="spriteUrl" />
+          <img :src="spriteUrl" @click="activeTab = 0" />
         </div>
       </b-tooltip>
     </figure>
 
     <div class="media-content">
-      <b-field grouped>
-        <!-- Name -->
-        <b-field label="Name">
-          <b-input ref="name" :value="name" @input="input"></b-input>
-        </b-field>
+      <b-tabs v-model="activeTab" type="is-toggle" expanded>
 
-        <!-- Mixins -->
-        <b-field label="Mixins" expanded>
-          <mixin-input ref="mixins" :value="mixins" @input="input" />
-        </b-field>
-      </b-field>
+        <b-tab-item label="Object Options" icon="settings">
+          <div class="media-content">
+            <b-field grouped>
+              <!-- Name -->
+              <b-field label="Name">
+                <b-input ref="name" :value="name" @input="input" />
+              </b-field>
 
-      <!-- Energy -->
-      <b-field :addons="false">
-        <label class="label">Energy <small>({{ energy }}%)</small></label>
-        <b-slider ref="energy" size="is-large" :value="energy" @input="input" rounded />
-      </b-field>
+              <!-- Mixins -->
+              <b-field label="Mixins" expanded>
+                <mixin-input ref="mixins" :value="mixins" @input="input" />
+              </b-field>
+            </b-field>
 
-      <object-mover :has-move-buttons="!isCreating" :object="$props" />
+            <!-- Energy -->
+            <b-field :addons="false">
+              <label class="label">Energy <small>({{ energy }}%)</small></label>
+              <b-slider ref="energy" size="is-large" :value="energy" @input="input" rounded />
+            </b-field>
 
-      <!-- Blocking -->
-      <b-field>
-        <b-switch ref="blocking" :value="blocking">
-          <strong>Blocking</strong>
-        </b-switch>
-      </b-field>
+            <object-mover :has-move-buttons="!isCreating" :object="$props" />
+
+            <!-- Blocking -->
+            <b-field>
+              <b-switch ref="blocking" :value="blocking" @input="input">
+                <strong>Blocking</strong>
+              </b-switch>
+            </b-field>
+          </div>
+        </b-tab-item>
+
+        <b-tab-item label="Change Sprite" icon="image">
+          <div class="media-content">
+            <sprite-input ref="sprite" v-model="internalSprite" :sprites="availableSprites" @input="input" />
+          </div>
+        </b-tab-item>
+      </b-tabs>
 
       <section class="level">
         <!-- Completion -->
         <div class="level-left">
           <div class="level-item">
             <section class="buttons">
-              <b-button>
-                Change Sprite
-              </b-button>
               <b-button type="is-success" :disabled="!canComplete" @click="complete">
                 <template v-if="isCreating">Create Object</template>
                 <template v-else>Apply Changes</template>
@@ -58,11 +68,14 @@
           <div class="level-item">
             <switch-button type="is-warning" @click="reset">Reset</switch-button>
           </div>
-          <slot name="bottom-right"></slot>
+          <div class="level-item" v-if="!isCreating">
+            <switch-button slot="bottom-right" type="is-danger" @click="remove">
+              Remove
+            </switch-button>
+          </div>
         </div>
       </section>
     </div>
-
   </article>
 </template>
 
@@ -77,15 +90,16 @@
 import _            from 'lodash';
 import Vue          from 'vue';
 import MixinInput   from '../Inputs/MixinInput';
+import SpriteInput  from '../Inputs/SpriteInput';
 import FormMixin    from '../Utilities/FormMixin';
 import SwitchButton from '../Utilities/SwitchButton'
-import ObjectMover from './ObjectMover';
+import ObjectMover  from './ObjectMover';
 
 import { SpriteBaseUrl, ObjectSprites } from '@/constants/sprites';
 
 
 export default {
-  components: { MixinInput, SwitchButton, ObjectMover },
+  components: { MixinInput, SwitchButton, ObjectMover, SpriteInput },
 
   mixins: [FormMixin],
 
@@ -105,23 +119,49 @@ export default {
     blocking : Boolean
   },
 
-
   computed: {
+    /**
+     * Array of all available object sprites.
+     *
+     * @return {array}
+     */
+    availableSprites() {
+      return ObjectSprites.map((sprite) => {
+        return {
+          title : sprite.title,
+          src   : SpriteBaseUrl + sprite.src
+        };
+      });
+    },
+
     /**
      * Get sprite of object with sprite path prefix.
      *
      * @return {string}
      */
     spriteUrl() {
-      return SpriteBaseUrl + this.sprite + '.png';
-    },
+      return SpriteBaseUrl + this.internalSprite + '.png';
+    }
   },
-
 
   data() {
     return {
-      updateKeys: ['name', 'mixins', 'energy', 'blocking']
+      activeTab: 0,
+      updateKeys: ['name', 'mixins', 'energy', 'blocking', 'sprite'],
+      spriteInputVisible: false,
+      internalSprite: this.sprite
     };
   },
+
+  methods: {
+    /**
+     *
+     *
+     * @return {void}
+     */
+    remove() {
+      this.$store.dispatch('objects/removeObject', { id: this.id });
+    }
+  }
 }
 </script>
