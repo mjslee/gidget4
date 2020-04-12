fca02d05b860bc8d3f2a5bef453adce5efb66a4e
thetarkus <mike@mikemob.com>
2020-04-12 04:12:11 -0400

    Add TileForm component

---
+++ frontend/components/Gidget/Tiles/TileForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <article>
+    <sprite-input v-model="internalSprite" :sprites="availableSprites" />
+  </article>
+</template>
+
+
+<script>
+import SpriteInput from '../Inputs/SpriteInput';
+import { SpriteBaseUrl, TileSprites } from '@/constants/sprites';
+
+
+export default {
+  components: {
+    SpriteInput
+  },
+
+  props: {
+    position: Object,
+    sprite: String
+  },
+
+  computed: {
+    /**
+     *
+     */
+    availableSprites() {
+      return TileSprites.map((sprite) => ({
+        title : sprite.title,
+        src   : SpriteBaseUrl + sprite.src
+      }));
+    },
+
+    /**
+     *
+     */
+    internalSprite: {
+      get() {
+        return this.sprite;
+      },
+      set(value) {
+        this.$emit('update:sprite', value);
+      }
+    }
+  }
+}
+</script>
