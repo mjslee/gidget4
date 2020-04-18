c2e06f97daba95c51f42d0eea68c4a2460c0f01d
thetarkus <mike@mikemob.com>
2020-04-17 23:37:29 -0400

    Add Literal component for display literals

---
+++ frontend/components/Gidget/Content/Literal.vue
@@ -0,0 +1,37 @@
+<template>
+  <span @click="togglePopover">
+    {{ value }}
+
+    <popover :active.sync="isPopoverActive" :element="this" />
+  </span>
+
+</template>
+
+
+<script>
+import Popover from './Popover'
+
+export default {
+  name: 'Literal',
+
+  components: {
+    Popover
+  },
+
+  props: {
+    value: Array | Object | String | Boolean | Number
+  },
+
+  data() {
+    return {
+      isPopoverActive: false
+    }
+  },
+
+  methods: {
+    togglePopover() {
+      this.isPopoverActive = !this.isPopoverActive;
+    }
+  }
+}
+</script>
