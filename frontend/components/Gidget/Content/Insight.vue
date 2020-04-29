e1cedb5c2e5da620cec4c8c0d2bfc68682a280dd
thetarkus <mike@mikemob.com>
2020-04-29 00:52:51 -0400

    (WIP) Add variable insight popover content

---
+++ frontend/components/Gidget/Content/Insight.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    {{ identifier }}: {{ value }}
+  </article>
+</template>
+
+
+<script>
+export default {
+  props: {
+    identifier: String,
+    value: Array | Object | String | Boolean | Number
+  }
+}
+</script>
